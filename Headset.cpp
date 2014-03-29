3599c8e57b2bb0fa3c84d3139d58284efc13166e
ashwinrag <ashwinrag@users.noreply.github.com>
2014-03-29 13:41:35 -0400

    Update Headset.cpp

--- Headset.cpp
+++ Headset.cpp
@@ -5,48 +5,30 @@ uint32_t Head::brainData(uint32_t* wave, uint8_t atten, uint8_t med){
   size_t len = 8;
   uint32_t result = 0;
   
-  //sum for the 
-  //low delta
-  result -= 2*wave[0];
-  //higher theta is focused, lower theta is daydreaming
-  result += wave[1]/2;
-  //lower alpha waves;
-  result -= wave[2] + wave[3];
-  //high beta waves
-  result += 2*(wave[4] + wave[5]);
-  //high gamma waves
-  result += wave[6] + wave[7];
+  //weighted sum of each freq
+  for (int i = 0; i < len; i++){
+    result += (i+1)*wave[i];
+  }
   
   result += atten;
   result -= med;
+  uint32_t max = wave[0];
+
+  //find how closely the waves are synced
+  uint32_t max = wave[0];
+  for (size_t i = 1; i < len; i++){
+	if (wave[i] > max)
+	  max = wave[i];
+  }
+  uint32_t min = wave[0];
+  for (size_t i = 1; i < len; i++){
+	if (wave[i] < min)
+	  min = wave[i];
+  }
   
-  //find how closely the waves are synchronized
-  uint32_t max = getMax(wave, len);
-  uint32_t min = getMin(wave, len);
-  uint32_t diff = max-min;
   //synced waves increase focus
-  result -= diff/min;
+  result -= 8*(max-min);
   
   //scale result based on how we're going to put together the binary string
-  return result % 1000000;  //change scaling factor as needed
-}
-
-//gets the min in the array
-uint32_t Head::getMin (uint32_t* array, size_t len){
-  uint32_t min = array[0];
-  for (size_t i = 1; i < len; i++){
-	if (array[i] < min)
-	  min = array[i];
-  }
-  return min;
-}
-
-//gets the max in the array
-uint32_t Head::getMax (uint32_t* array, size_t len){
-  uint32_t max = array[0];
-  for (size_t i = 1; i < len; i++){
-	if (array[i] > max)
-	  max = array[i];
-  }
-  return max;
+  return result % 100000000;  //change scaling factor as needed
 }
