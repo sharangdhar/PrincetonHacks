a657fee4aabdfd29eba39d208856df32c9bc0eab
sohilshah2 <sohils@cmu.edu>
2014-03-29 06:49:15 -0400

    Update BrainWave.cpp

--- BrainWave.cpp
+++ BrainWave.cpp
@@ -25,15 +25,5 @@ uint8_t meditation() {
 }
 
 uint32_t* brainData() {
-	uint32_t* waves = malloc(8*sizeof(uint32_t));
-	waves[0] = b.readDelta(); 
-	waves[1] = b.readTheta();
-	waves[2] = b.readLowAlpha();
-	waves[3] = b.readHighAlpha();
-	waves[4] = b.readLowBeat();
-	waves[5] = b.readHighBeta();
-	waves[6] = b.readLowGamma();
-	waves[7] = b.readHighGamma();
-	return waves;
-	//return b.readPowerArray();
+	return b.readPowerArray();
 }
